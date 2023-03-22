8998b47cc3d4636cbbf3900c1fe8a48f20c82f64
kalexoudis <k.alexoudis@tum.de>
2023-03-22 16:25:41 +0100

    added argparse

--- alert.py
+++ alert.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import argparse
 from playsound import playsound
 
 
@@ -12,8 +13,18 @@ def get_bitcoin_price(currency='USD'):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-C', '--currency', type=str, default='USD',
+                        help='Currency to check the price. (Default: USD)')
+    parser.add_argument('-M', '--min', type=float, default='30000',
+                        help='Minimum price to alert. (Default: 30000)')
+    parser.add_argument('-T', '--time', type=float, default='10',
+                        help='Time in seconds to check the price. (Default: 10)')
+
+    args = parser.parse_args()
+
     while True:
-        print(get_bitcoin_price())
-        if get_bitcoin_price() < 30000:
+        if get_bitcoin_price(args.currency) < args.min:
             playsound('./you-suffer.mp3')
-            time.sleep(10)
+
+        time.sleep(args.time)
